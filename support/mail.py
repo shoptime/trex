6c0f791308fbaf01ddb2e36c0702d33ff50d35aa
Nigel McNie <nigel@mcnie.name>
2013-01-26 08:39:11 +1300

    Added mail module

---
+++ support/mail.py
@@ -0,0 +1,127 @@
+"""
+Email abstraction, although it's pretty thin over postmark
+
+To send:
+
+import support.mail as mail
+mail.send(to="nigel@shoptime.co.nz", subject="Hai!", text_body="Your preference regarding mudkipz?")
+
+See mail.tpl_send() to send HTML mail.
+
+Errors will generate exceptions as per https://github.com/themartorana/python-postmark/ - this lib does not
+attempt to wrap those at this time.
+
+"""
+
+from __future__ import absolute_import
+import postmark
+from postmark.core import PMMailUnprocessableEntityException
+import pickle
+from trex.flask import app
+from mongoengine import Document, StringField
+from jinja2 import Environment, PackageLoader
+from flask import url_for
+from premailer import transform
+import re
+
+
+# Jinja env for templated emails
+env = Environment(loader=PackageLoader(app.import_name, 'templates/email'))
+env.globals['url_for'] = url_for
+
+
+# Signals whether we're sending emails or just capturing them to mongo (for the
+# test suite mainly)
+capturing = False
+
+
+def send(sender=None, to=None, cc=None, bcc=None, reply_to=None, subject=None, tag=None, html_body=None, text_body=None, custom_headers=None, attachments=None, test=False):
+    """
+    Send email immediately. If postmark.test_override is set in the config then it will always generate test mode regardless
+    of the test setting
+
+    @param sender: Email from, defaults to configuration choice
+    @param to: Email to, "name@email.com" or "First Last <name@email.com>" format
+    @param cc: Email cc
+    @param bcc: Email bcc
+    @param reply_to: Email reply to, "name@email.com" or "First Last <name@email.com>" format
+    @param subject: Subject line
+    @param tag: PostMark/Processor tags
+    @param html_body: HTML body
+    @param text_body: Text body
+    @param custom_headers: custom headers as a dictionary of key=value
+    @param attachments: A list of tuples or email.mime.base.MIMEBase objects as attachments
+    @param test: Whether to simply dump the results in the log instead of sending.
+    @return:
+    """
+
+    api_key = app.settings.get('postmark', 'api_key')
+    sender  = sender or app.settings.get('postmark', 'sender')
+    test    = test or app.settings.getboolean('postmark', 'test')
+    if not reply_to and 'reply_to' in app.settings.options('postmark'):
+        reply_to = app.settings.get('postmark', 'reply_to')
+
+    if custom_headers is None:
+        custom_headers = {}
+    if attachments is None:
+        attachments = []
+
+    pm = postmark.PMMail(api_key=api_key, sender=sender, to=to, cc=cc, bcc=bcc, reply_to=reply_to, subject=subject, tag=tag, html_body=html_body, text_body=text_body, custom_headers=custom_headers, attachments=attachments)
+
+    if capturing or app.in_test_mode:
+        doc = CapturedEmail()
+        doc.postmark_obj = pickle.dumps(pm)
+        doc.save()
+
+    if test:
+        test = True
+
+    pm.send(test=test)
+
+def html_sample(template, tplvars):
+    html_template = env.get_template('%s-html.jinja2' % template)
+    return html_to_emailhtml(html_template.render(tplvars))
+
+def text_sample(template, tplvars):
+    text_template = env.get_template('%s-text.jinja2' % template)
+    return text_template.render(tplvars)
+
+def tpl_send(template, *args, **kwargs):
+    # TODO make a template cache?
+
+    tplvars = kwargs['tplvars']
+    del kwargs['tplvars']
+
+    html_template = env.get_template('%s-html.jinja2' % template)
+    kwargs['html_body'] = html_to_emailhtml(html_template.render(tplvars))
+
+    text_template = env.get_template('%s-text.jinja2' % template)
+    kwargs['text_body'] = text_template.render(tplvars)
+
+    return send(*args, **kwargs)
+
+def html_to_emailhtml(html):
+    emailhtml = transform(html, base_url=app.settings.get('server', 'url'))
+
+    # Remove <head> node that is utterly useless (email clients ignore/mess up)
+    emailhtml = re.sub(r'<head>.*</head>', '', emailhtml)
+    # Add semi-colon after last style rule in style attrs - some email clients need this
+    emailhtml = re.sub(r'(style="[^"]*)"', r'\1;"', emailhtml)
+
+    return emailhtml
+
+def begin_capturing():
+    global capturing
+    capturing = True
+    CapturedEmail.objects.delete()
+
+def end_capturing():
+    global capturing
+    capturing = False
+    return [ x.get_postmark_obj() for x in CapturedEmail.objects.all() ]
+
+class CapturedEmail(Document):
+    postmark_obj = StringField(required=True)
+
+    def get_postmark_obj(self):
+        return pickle.loads(str(self.postmark_obj))
