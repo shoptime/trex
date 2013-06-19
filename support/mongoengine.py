98a4c410a29609df80406c3bfc027aa58b52f4c9
Martyn Smith <martyn@dollyfish.net.nz>
2013-06-20 09:26:03 +1200

    Generic file serving code for gridfs files

--- support/mongoengine.py
+++ support/mongoengine.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 from mongoengine import EmailField
+from flask import request, abort
 
 class LowerCaseEmailField(EmailField):
     def to_mongo(self, *args, **kwargs):
@@ -7,3 +8,25 @@ class LowerCaseEmailField(EmailField):
 
     def prepare_query_value(self, *args, **kwargs):
         return super(self.__class__, self).prepare_query_value(*args, **kwargs).lower()
+
+def serve_file(document, field, index=None, set_filename=True):
+    from app import app
+    try:
+        file = getattr(document, field)
+        if index is not None:
+            file = file[int(index)]
+    except IndexError:
+        abort(404)
+
+    if 'If-None-Match' in request.headers and request.headers['If-None-Match'] == str(file._id):
+        return app.response_class('', 304)
+
+    response = app.response_class(file.get(), 200)
+    response.headers['Content-Type'] = file.content_type
+    response.headers['ETag'] = str(file._id)
+    response.headers['Cache-Control'] = 'private, max-age=31622400'
+
+    if set_filename:
+        response.headers['Content-Disposition'] = 'filename=%s' % file.filename
+
+    return response
