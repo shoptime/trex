120b6da8c00d09ab7a89cf21aa22fdd17f7bb200
Martyn Smith <martyn@dollyfish.net.nz>
2013-06-24 09:38:49 +1200

    Support for force audit lines to be system actions

--- support/audit.py
+++ support/audit.py
@@ -3,11 +3,14 @@
 from __future__ import absolute_import
 from flask import g
 
-def audit(description, tags, documents=None, user=None):
+def audit(description, tags, documents=None, user=None, system_action=False):
     import app.model as m  # We import this late so that the audit method can be loaded into the model
 
     if user is None and hasattr(g, 'user'):
         user = g.user
+
+    if system_action:
+        user = None
 
     if documents is None:
         documents = []
