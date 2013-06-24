30fd74c1e4a3805a04424a7745766a98950c408c
Nigel McNie <nigel@mcnie.name>
2013-06-24 15:16:29 +1200

    Minor cleanup to -t switch for app test

--- support/run.py
+++ support/run.py
@@ -96,7 +96,7 @@ class Manager(script.Manager):
             return context
 
         @self.option('-w', '--wait', action='store_true', default=False, help='Wait for keypress on test failure')
-        @self.option('-t', '--test', action='store', default=False, help='Execute specific test')
+        @self.option('-t', '--test', action='store', default=[], help='Execute specific test')
         def test(wait=False, test=None):
             "Run the test suite"
             t = TestRunner(wait_after_exception=wait)
