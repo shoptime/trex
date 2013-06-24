b141ccb13ae8bcc2ed58a966d03a6c2eb8ba80a0
Martyn Smith <martyn@dollyfish.net.nz>
2013-06-24 14:39:04 +1200

    Audit log is now paged

--- support/blueprint_audit_log.py
+++ support/blueprint_audit_log.py
@@ -4,17 +4,22 @@ from __future__ import absolute_import
 from trex.flask import app
 from trex.flask import AuthBlueprint, render_html
 from .. import auth
-from flask import abort, redirect, url_for
-from flask.ext import wtf
-from trex.support import token
-from .audit import audit
+from flask import request
 import app.model as m
+from .pager import Pager
 
 blueprint = AuthBlueprint('trex.audit_log', __name__, url_prefix='/admin/audit')
 
 @blueprint.route('/', auth=auth.has_flag('trex.audit_log'))
 @render_html('trex/audit_log/index.jinja2')
 def index():
-    return dict(entries=m.Audit.objects)
+    pager = Pager(
+        m.Audit.objects.count(),
+        page     = int(request.args.get('page', 1)),
+        base_uri = request.url,
+        per_page = 10,
+    )
+
+    return dict(pager=pager, entries=m.Audit.objects.skip(pager.skip()).limit(pager.limit()))
 
 app.register_blueprint(blueprint)
