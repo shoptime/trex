5a850de2502a923d0333545da46d730ce6ed9a8a
Nigel McNie <nigel@mcnie.name>
2013-09-04 12:09:48 +1200

    Patch the timepicker to allow us to set a more reasonable default time

--- cdn_overlay/js/form-timepicker.js
+++ cdn_overlay/js/form-timepicker.js
@@ -14,6 +14,7 @@
  *   step: # of minutes to step the time by
  *   startTime: beginning of the range of acceptable times
  *   endTime: end of the range of acceptable times
+ *   defaultTime: time to open the picker to if no time is currently selected
  *   separator: separator string to use between hours and minutes (e.g. ':')
  *   show24Hours: use a 24-hour scheme
  */
@@ -41,6 +42,7 @@
     var keyDown = false;
     var startTime = timeToDate(settings.startTime, settings);
     var endTime = timeToDate(settings.endTime, settings);
+    var defaultTime = timeToDate(settings.defaultTime, settings);
     var selectedClass = "selected";
     var selectedSelector = "li." + selectedClass;
 
@@ -99,7 +101,7 @@
       $tpDiv.show();
 
       // Try to find a time in the list that matches the entered time.
-      var time = elm.value ? timeStringToDate(elm.value, settings) : startTime;
+      var time = elm.value ? timeStringToDate(elm.value, settings) : defaultTime;
       var startMin = startTime.getHours() * 60 + startTime.getMinutes();
       var min = (time.getHours() * 60 + time.getMinutes()) - startMin;
       var steps = Math.round(min / settings.step);
@@ -211,6 +213,7 @@
     step:30,
     startTime: new Date(0, 0, 0, 0, 0, 0),
     endTime: new Date(0, 0, 0, 23, 30, 0),
+    defaultTime: new Date(0, 0, 0, 8, 0, 0),
     separator: ':',
     show24Hours: true
   };
