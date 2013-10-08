f39f13ca38da7ee96018a2202c5e1fcf30143113
Martyn Smith <martyn@dollyfish.net.nz>
2013-10-09 11:03:00 +1300

    rubble form.fill supports radios

--- rubble/helpers/form.py
+++ rubble/helpers/form.py
@@ -7,10 +7,15 @@ from .assertions import *
 
 def fill(**kwargs):
     for key, value in kwargs.items():
-        el = find('[name="%s"]' % key).length_is(1, message="Couldn't find form element: %s" % key)
-        if el.tag_name() == 'select':
+        el = find('[name="%s"]' % key)
+        is_equal(el.length() > 0, True, "Couldn't find form element: %s" % key)
+        if el[0].tag_name() == 'select':
             el.select_by_value(value)
-        elif el.attr('type') == 'checkbox':
+        elif el[0].attr('type') == 'radio':
+            el = find('[name="%s"][value="%s"]' % (key, value))
+            el.length_is(1, message="Couldn't find radio button %s=%s" % (key, value))
+            el.click()
+        elif el[0].attr('type') == 'checkbox':
             for subel in el.filter_by_selected():
                 subel.click()
             el = find('[name="%s"][value="%s"]' % (key, value))
