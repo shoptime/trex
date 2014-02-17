7a760c532028604e883948b4e7b8f2026dcfd47a
Nigel McNie <nigel@mcnie.name>
2014-02-17 15:59:21 +1300

    Add an "incremental check" mode to rubble's mail.check().
    This allows you to check that some emails have been captured, without having to check for all of them.

--- rubble/helpers/mail.py
+++ rubble/helpers/mail.py
@@ -14,24 +14,50 @@ def assert_no_unchecked_emails():
 def begin_capturing():
     assert_no_unchecked_emails()
 
-def check(*matches):
-    message("Checking emails")
+def check(*matches, **kwargs):
+    """Checks what emails the system has sent.
+
+    Pass a series of dicts, one for each email you wish to check. For details on what keys the dict can have for
+    matching checks, see compare().
+
+    Checking has two modes: "absolute" (the default), and "incremental".
+
+    In absolute mode, you must pass a dict describing every single email the system has sent since
+    begin_capturing() was called.
+
+    In incremental mode, you can pass dicts describing any of the emails that have been sent. You don't have to
+    describe them all. Emails that are matched will be taken off the list to be checked, allowing you to do this:
+
+        mail.begin_capturing()
+        operation_that_sends_four_emails()
+        mail.check(incremental=True, dict(... one of the emails...))
+        mail.check(incremental=True, dict(... one of the other emails...))
+        mail.check(dict(... one of the last two ...), dict(... the last one left ...))
+    """
+
+    incremental = kwargs.get('incremental', False)
+    message("Checking emails (incremental=%s)" % incremental)
 
     emails = set(CapturedEmail.objects)
 
-    is_equal(len(emails), len(matches), "Correct number of emails")
+    if not incremental:
+        is_equal(len(emails), len(matches), "Correct number of emails")
+
+    emails_to_delete = set()
 
     for match in matches:
         found_match = False
         for email in emails:
             if compare(email, match):
                 emails.remove(email)
+                emails_to_delete.add(email.id)
                 found_match = True
                 break
         if not found_match:
             harness().handle_error(TestFailureException("Failed to find match for specified email", observed=[x.as_string() for x in emails], expected=match))
 
-    CapturedEmail.objects.delete()
+    for email_id in emails_to_delete:
+        CapturedEmail.objects.get(id=email_id).delete()
 
     return emails
 
