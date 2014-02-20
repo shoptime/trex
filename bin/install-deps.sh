0ec849ab3ac747b0090cf5b0d0fef068f71d6743
Martyn Smith <martyn@dollyfish.net.nz>
2014-02-20 16:05:01 +1300

    CHANGE: new install-deps script (does bower stuff etc)

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -7,21 +7,18 @@ PROJECT_ROOT=$(cd $(dirname "$0")/../..; pwd)
 cd "$PROJECT_ROOT"
 . bin/activate
 
+
 for file in {,trex/}requirements.txt; do [ -f $file ] && pip install -r $file; done
 if [ "$1" == "-d" ]; then
     pip install -r trex/dev-requirements.txt
 fi
 
+[ -f package.json ] || cp trex/package.json package.json
 mkdir -p node_modules
-for file in {,trex/}node-requirements.txt; do
-    if [ -f $file ]; then
-        xargs npm install <$file
-    fi
-done
+npm install
 
-for script in node_modules/.bin/*; do
-    script=$(basename $script)
-    ln -f -s ../node_modules/.bin/lessc bin/
-done
+[ -f bower.json ] || cp trex/bower.json bower.json
+[ -f .bowerrc ] || cp trex/.bowerrc .bowerrc
+node_modules/.bin/bower install
 
 exit 0
