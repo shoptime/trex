a26e2f1a6b9bace801c84c287bbbde9bc824eb4a
Nigel McNie <nigel@mcnie.name>
2014-03-05 14:50:54 +1300

    Import app so we can get at settings properly.

--- support/run.py
+++ support/run.py
@@ -8,6 +8,7 @@ if 'VIRTUAL_ENV' not in os.environ:
     activate_this = os.path.join(project_root, 'bin', 'activate_this.py')
     execfile(activate_this, dict(__file__=activate_this))
 
+from app import app
 from flask.ext import script
 import imp
 import inspect
