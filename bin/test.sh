358b1d05c0b4bc61f0db877dfbae17e4c38a7b04
Nigel McNie <nigel@mcnie.name>
2014-05-23 12:54:51 +1200

    [CHANGE] test.sh now accepts commit to test from stdin; only supports rubble.
    Now you can pass -s to test.sh, which will make it read key=value pairs from
    stdin. When stdin is closed, the script will continue as normal. The only
    key/value pair that is relevant right now is:
    
        DS_COMMIT=<git commit hash>
    
    Which now causes this script to do a git checkout at that commit hash, rather
    than a git pull.
    
    Also, passing 'rubble' as an argument is now redundant. The rubble test
    framework is always triggered.

--- bin/test.sh
+++ bin/test.sh
@@ -1,18 +1,59 @@
 #!/bin/bash
 set -e
+shopt -s extglob
 
-if [ "$1" == "" ]; then
-    test_harness='test'
-else
-    test_harness="$1"
+# Parse CLI
+config_from_stdin=false
+test_harness_opts=''
+
+[ "$1" == "rubble" ] && shift   # backward compatibility with old invocations of this script
+
+OPTIND=1
+while getopts "sp:" opt; do
+    case "$opt" in
+        s)
+            config_from_stdin=true
+            ;;
+        p)
+            test_harness_opts="$test_harness_opts -p$OPTARG"
+            ;;
+        \?)
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+[ "%1" = "--" ] && shift
+
+# Read config if asked for
+if $config_from_stdin; then
+    NORMAL_IFS="$IFS"
+    IFS="="
+    while read key val; do
+        case "$key" in
+            DS_*([a-zA-Z_]) )
+                IFS="$NORMAL_IFS"
+                declare $key="$val"  2>/dev/null || echo "Bad key"
+                IFS="="
+                ;;
+            *)
+                echo "Bad key"
+                ;;
+        esac
+    done
 fi
 
 # Basic prep
 source bin/activate
 mkdir -p logs
 
-# Get the latest code (should be git checkout when we can)
-git pull
+# Get the latest code
+if [ "$DS_COMMIT" != "" ]; then
+    git fetch
+    git checkout "$DS_COMMIT"
+else
+    git pull
+fi
 
 TREX_OLD_COMMIT=$(cd trex && git rev-parse HEAD)
 git submodule update --init
@@ -34,12 +75,8 @@ bash trex/bin/install-deps.sh
 app compile_static
 
 # Run tests
-echo "Executing app $test_harness" "${*:2}"
-if [ "$2" != "" ]; then
-    app "$test_harness" "${*:2}"
-else
-    app "$test_harness"
-fi
+echo "Executing app rubble$test_harness_opts"
+app rubble $test_harness_opts
 
 # Reload for other things that hit this hive instance
 touch site.wsgi
