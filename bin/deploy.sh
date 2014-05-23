1e6439d7215aa24eb3d11f20c93f2b5f693745d4
Nigel McNie <nigel@mcnie.name>
2014-05-23 15:34:10 +1200

    Make deploy.sh read config from deployswitch to deploy code at specific commit.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
 set -e
+shopt -s extglob
+
+# Parse CLI
+config_from_deployswitch=false
+test_harness_opts=''
+
+OPTIND=1
+while getopts "d" opt; do
+    case "$opt" in
+        d)
+            config_from_deployswitch=true
+            ;;
+        \?)
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+[ "%1" = "--" ] && shift
+
+# Read config from deployswitch if asked for
+if $config_from_deployswitch; then
+    NORMAL_IFS="$IFS"
+    IFS="="
+    while read key val; do
+        case "$key" in
+            DS_*([a-zA-Z_]) )
+                IFS="$NORMAL_IFS"
+                declare $key="$val"  2>/dev/null || echo "Bad key"
+                IFS="="
+                ;;
+            .)
+                # A single '.' is the EOF marker
+                break
+                ;;
+            *)
+                echo "Bad key"
+                ;;
+        esac
+    done
+fi
 
 # Basic prep
 source bin/activate
@@ -7,7 +48,12 @@ mkdir -p logs
 
 # Get the latest code (should be git checkout when we can)
 OLD_COMMIT=$(git rev-parse HEAD)
-git pull
+if [ "$DS_DEPLOY_HASH" != "" ]; then
+    git fetch
+    git checkout "$DS_DEPLOY_HASH"
+else
+    git pull
+fi
 NEW_COMMIT=$(git rev-parse HEAD)
 git submodule update --init
 
