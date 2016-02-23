2a4d165a02694319854a8dae392864a9a02647bc
Martyn Smith <martyn@dollyfish.net.nz>
2016-02-24 10:53:07 +1300

    geturl method for ConfigParser

--- support/configparser.py
+++ support/configparser.py
@@ -3,8 +3,13 @@
 from __future__ import absolute_import
 
 from ConfigParser import ConfigParser
+from furl import furl
 
 class TrexConfigParser(ConfigParser):
     def getlist(self, *args, **kwargs):
         value = self.get(*args, **kwargs)
-        return [ x.strip() for x in value.split(',') ]
+        return [x.strip() for x in value.split(',')]
+
+    def geturl(self, *args, **kwargs):
+        value = self.get(*args, **kwargs)
+        return furl(value)
